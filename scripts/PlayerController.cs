9e4fdce5f5ab008494ee8e6364634272fd4d8664
agrimavij <77989480+agrimavij@users.noreply.github.com>
2022-10-06 10:52:09 +0100

    Add files via upload

---
+++ scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic; using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerController : MonoBehaviour { 
+    public float speed;
+    private int count;
+    private int numPickups = 8; //Put here the number of pickups you have. public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+    public Vector2 moveValue;
+void Start () {
+count = 0; winText.text = "";
+SetCountText();
+}
+
+void FixedUpdate () {
+Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+GetComponent <Rigidbody >().AddForce(movement * speed * Time. fixedDeltaTime);
+} 
+
+void OnTriggerEnter(Collider other) {
+    if(other.gameObject.tag == "PickUp") {
+other.gameObject.SetActive(false); count ++;
+SetCountText();
+} }
+private void SetCountText () {
+scoreText.text = "Score: " + count.ToString(); if(count >= numPickups) {
+winText.text = "You win!"; }
+}
+}
